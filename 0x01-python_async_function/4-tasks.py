bbd45f12653ac72a399f7c75f6e991f79a346e07
Bigizic <olalekanisaac75@gmail.com>
2023-12-11 18:26:59 +0000

    add new files

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,6 +2,21 @@
 """ alters a function
 """
 
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-"""Implementation """
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Implementation """
+    spawns = [task_wait_random(max_delay) for _ in range(n)]
+    # task = [asyncio.create_task(x) for x in spawns]
+
+    result = await asyncio.gather(*spawns)
+    new_list = []
+    while result:
+        min_e = min(result)
+        new_list.append(min_e)
+        result.remove(min_e)
+
+    return new_list
