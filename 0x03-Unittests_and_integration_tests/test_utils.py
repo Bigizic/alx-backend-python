cbda67d896d8dca7da5d3eafbfeb22a4853d2a39
Bigizic <olalekanisaac75@gmail.com>
2024-01-06 00:23:33 +0000

    add updates

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """Unittest module for utils, fixtures and client
 """
 
-from parameterized import parameterized, parameterized_class
+from parameterized import parameterized
 from typing import Dict, List, Tuple, Union
 import unittest
 from utils import access_nested_map as anm
@@ -17,11 +17,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(
-            self,
-            n_map: Dict,
-            pa: Tuple[str],
-            expected: Union[Dict, int]
-            ) -> None:
+    def test_access_nested_map(self, n_map: Dict, pa: Tuple[str],
+                               expect: Union[Dict, int]) -> None:
         """Tests the access_nested_map method from utils"""
-        self.assertEqual(anm(n_map, pa), expected)
+        self.assertEqual(anm(n_map, pa), expect)
