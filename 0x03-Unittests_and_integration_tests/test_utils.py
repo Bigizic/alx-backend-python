6f61165afd64b5340465b8777088e181b175dfa6
Bigizic <olalekanisaac75@gmail.com>
2024-01-06 03:14:38 +0000

    add two tasks

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,9 @@
 from parameterized import parameterized
 from typing import Dict, List, Tuple, Union
 import unittest
+from unittest.mock import MagicMock, patch, Mock
 from utils import access_nested_map as anm
+from utils import get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -31,3 +33,45 @@ class TestAccessNestedMap(unittest.TestCase):
         """Handles utils.access_nested_map exceptions"""
         with self.assertRaises(KeyError):
             anm(n_map, pa)
+
+
+class TestGetJson(unittest.TestCase):
+    """Implementation of the utils.get_json unittests
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url: str, py: Union[Dict, bool]) -> None:
+        """Test that utils.get_json returns the expected result when passed
+        the url
+        """
+        hi = {'json.return_value': py}
+        with patch("requests.get", return_value=Mock(**hi)) as get_req:
+            self.assertEqual(get_json(url), py)
+            get_req.assert_called_once_with(url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Implementation of the utils.memoize unittests
+    """
+
+    def test_memoize(self) -> None:
+        """test that correct result is returned when calling a_property and
+        a_method
+        """
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method",
+                          return_value=lambda: 42) as hi:
+            test = TestClass()
+            self.assertEqual(test.a_property(), 42)
+            self.assertEqual(test.a_property(), 42)
+            hi.assert_called_once()
